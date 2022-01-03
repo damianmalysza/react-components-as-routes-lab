14bc537380ace5698e3fec6f23fa99200a043759
Damian Malysza <damian.malysza@gmail.com>
2022-01-03 11:49:43 -0500

    commit tree fabfa9350c3fcb09efce38872610cd27c1d40f9b

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { movies } from '../data';
 
+const renderMovies = () => {
+  return movies.map(movie => {
+    return (
+    <div>
+      {movie.title} {movie.time}
+      <ul>
+        {movie.genres.map(genre => <li>{genre}</li>)}
+      </ul>
+    </div>)
+  })
+}
+
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
